c75693f8254a90f119c25292a088426a7cf87efe
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2022-12-30 18:33:43 +0100

    Fix: Styling Starting page styel#

--- includes/startingPape.php
+++ includes/startingPape.php
@@ -7,7 +7,7 @@
         <p> Email: Bernardo.correia.alves@student.nhlstenden.com</p>
 
     </section>
-    <div class="img">
+    <div class="imgOfMe">
     <img src="./img/me.jpeg" alt="portrait of me" class="imgMe">
     </div>
 
@@ -22,9 +22,9 @@
         <p>If I reflect on period 1 I was expeting more of me. I was kinda sad that I was not able to pass php exam.
         Personally, I need to start investing more time into Css as it where I lack the most. This now leeds to me have to pass 2 exams insted of only 1 in period 2
         Something I know I can do.</p>
-        Overall I am really happy with my choice of coming to NHL Stenden. I enjoy the teachers I enjoy the university. As mention before
+        <p> Overall I am really happy with my choice of coming to NHL Stenden. I enjoy the teachers I enjoy the university. As mention before
         this for me is just a prepartion for my master in Networking. I have a lot of prior knwolege in what we are learing, this helps me take things easier
-        I was looking forward for the networking class and the upcoming JAVA ones are going to be fun
+        I was looking forward for the networking class and the upcoming JAVA ones are going to be fun</p>
         <p>Study progress has been better in period 2. I have made every assigment on week and on time. Project 1 has been passed with 8.0.
         Information management passed.</p>
         <p>I will definitely continue with my studies here in NHL Stenden. I still believe this was the choice to make after everything</p>
